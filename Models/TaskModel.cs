6e6cb670c665fffae6854f8e644e52c569ebad52
Pooja Singh <pusingh1304@gmail.com>
2025-08-08 08:40:05 +0530

    Initial commit -oritso Task Management App

---
+++ Models/TaskModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementApp.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public  string Title { get; set; }
+        public string Description { get; set; }
+        [Display(Name ="Due Date")]
+        public DateTime DueDate { get; set; }
+        public  string status  { get; set; }
+        public string Remarks { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string CreatedByName { get; set; }
+        public string   CreatedById { get; set; }
+        public string UpdatedByName { get; set; }
+        public string UpdatedById { get; set; }
+    }
+}
